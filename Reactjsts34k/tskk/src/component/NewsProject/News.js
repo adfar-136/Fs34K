34f6b0ddbe9f88254d9c46cf73e4bfd78f009eba
adfar-136 <adfarrasheed136@gmail.com>
2023-06-24 14:51:37 +0530

    done

--- Reactjsts34k/tskk/src/component/NewsProject/News.js
+++ Reactjsts34k/tskk/src/component/NewsProject/News.js
@@ -1,30 +1,58 @@
-import React, { useEffect, useState } from 'react'
-import Newsitem from './Newsitem'
+import React, { useEffect, useState } from 'react';
+import Newsitem from './Newsitem';
+import './News.css';
 
 export default function News() {
-    const [news,setNews]  =useState([])
-    const getNews=async()=>{
-      let data =await fetch("https://newsapi.org/v2/everything?q=tesla&from=2023-05-24&sortBy=publishedAt&apiKey=28a2c6b3aa1945c2afd8012e42fdac9e")
-      const jsonData =await data.json()
-      setNews(jsonData.articles)
-      console.log(news)
-    }
-    useEffect(()=>{
-       getNews()
-    })
+  const [news, setNews] = useState([]);
+  const [searchnews, setSearchnews] = useState('');
+
+  const getNews = async () => {
+    let data = await fetch(
+      `https://newsapi.org/v2/everything?q=${searchnews}&from=2023-05-24&sortBy=publishedAt&apiKey=d4ed6824421249c6b8c19bbc55e5b6ae`
+    );
+    const jsonData = await data.json();
+    setNews(jsonData.articles);
+  };
+
+  useEffect(() => {
+    getNews();
+  }, [searchnews]);
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    getNews();
+  };
+
   return (
-    <div className='container'>
-        <h2>Adfar News Network</h2>
-        <div className="row">
-        {news.map((item,index)=>{
-            return <div className="col-md-4">
-                <Newsitem title={item.title.slice(0,20)} description={item.description.slice(0,80)} image={item.urlToImage} url={item.url}/>
-            </div>
-        })}
-        </div>
-       
-      
-       
+    <div className="container">
+      <header className="header">
+        <h1 className="header-title">Adfar News Network</h1>
+        <form className="search-form" onSubmit={handleSearch}>
+          <input
+            className="search-input"
+            type="search"
+            placeholder="Search news"
+            value={searchnews}
+            onChange={(e) => setSearchnews(e.target.value)}
+          />
+          <button className="search-button" type="submit">
+            Search
+          </button>
+        </form>
+      </header>
+    {news && (
+      <div className="row">
+        {news.map((item, index) => (
+          <div className="col-md-4" key={index}>
+            <Newsitem
+              title={item.title?item.title.slice(0,20):""}
+              description={item.description?item.description.slice(0,75):""}
+              image={item.urlToImage?item.urlToImage:"https://techcrunch.com/wp-content/uploads/2023/04/GettyImages-1251935804.jpg?resize=1200,798"}
+              url={item.url}
+            />
+          </div>
+        ))}
+      </div>)}
     </div>
-  )
+  );
 }
--- Reactjsts34k/tskk/src/component/NewsProject/Newsitem.js
+++ Reactjsts34k/tskk/src/component/NewsProject/Newsitem.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import "./Newsitem.css"
 export default function Newsitem(props) {
   return (
     <div>
@@ -8,7 +8,7 @@ export default function Newsitem(props) {
   <div className="card-body">
     <h5 className="card-title">{props.title}</h5>
     <p className="card-text">{props.description}</p>
-    <a href={props.url} target='_blank' className="btn btn-primary">Read More</a>
+    <a href={props.url} target='_blank' rel='noreferrer' className="btn btn-primary">Read More</a>
   </div>
 </div>
     </div>
