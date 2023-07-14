91998c8e385fe606b1e02524a2d6acb054e84a53
adfar-136 <adfarrasheed136@gmail.com>
2023-07-14 14:32:18 +0530

    done

--- Reactjsts34k/tskk/src/App.js
+++ Reactjsts34k/tskk/src/App.js
@@ -1,10 +1,37 @@
 import React from 'react'
-import Todos from './component/Todo Main Project/Todos'
+import { useState } from 'react'
+import { useEffect } from 'react'
 
 export default function App() {
+  const [count,setCount] = useState(0)
+  const [run,setRun] = useState(true)
+  useEffect(()=>{
+    let interval;
+    if(run){
+    interval = setInterval(()=>{
+      setCount(count+1)
+    },1000)
+  }
+    return ()=>{
+      clearInterval(interval)
+    }
+  })
+  const handlePause=()=>{
+    setRun(false)
+  }
+  const handleResume =()=>{
+    setRun(true)
+  }
+  const handleReset= ()=> {
+    setCount(0)
+  }
   return (
     <div>
-      <Todos/>
+      <h1>Count : {count}</h1>
+      <button onClick={handlePause}>Pause</button>
+      <button onClick={handleResume}>Resume</button>
+
+      <button onClick={handleReset}>Reset</button>
     </div>
   )
 }
